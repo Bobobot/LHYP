63a4bbb4a47cc18cc284f7c9cf0c036ce19cb654
Bobobot <bonifac.solyom@gmail.com>
2020-12-05 19:00:54 +0100

    Added file manager to properly separate files
    seperates files into three different folders (train, validate, test), pays attention to hypertrophy ratios

---
+++ hyp_file_man.py
@@ -0,0 +1,71 @@
+import os
+import pickle
+import random
+import shutil
+
+
+class HypertrophyFileManager:
+	def separate_files(self, data_folder, percentages, folder_names, max_acceptable_ratio_difference):
+		pickle_files = sorted(os.listdir(data_folder))
+		pickle_files = [file for file in pickle_files if file.endswith(".rick")]
+
+		if len(percentages) != len(folder_names):
+			print("ERROR: percentage and folder name list must be the same length")
+			return
+		if sum(percentages) != 100:
+			print("ERROR: sum of percentages must be 100")
+			return
+
+		for folder_name in folder_names:
+			folder_path = os.path.join(data_folder, folder_name)
+			if not os.path.exists(folder_path):
+				os.makedirs(folder_path)
+
+		ratios_good = False
+		attempt_count = 0
+		list_of_file_lists = []
+		while not ratios_good:
+			list_of_file_lists.clear()
+			ratios_good = True
+			print(f'attempt {attempt_count + 1}')
+			attempt_count += 1
+
+			list_of_ratios = []
+			for perc in percentages:
+				list_of_ratios.clear()
+				list_of_file_lists.append(random.sample(pickle_files, round(len(pickle_files) * (perc / 100))))
+			for file_list in list_of_file_lists:
+				_, _, ratio = self.check_ratio_list(data_folder, file_list)
+				list_of_ratios.append(ratio)
+				if not (0.5 - max_acceptable_ratio_difference < ratio < 0.5 + max_acceptable_ratio_difference):
+					ratios_good = False
+					break
+			if ratios_good:
+				print("Final ratios:")
+				for i, folder_name in enumerate(folder_names):
+					print(f'{folder_name}: {list_of_ratios[i]}')
+
+		for folder_index, file_list in enumerate(list_of_file_lists):
+			for file_name in file_list:
+				file_path = os.path.join(data_folder, file_name)
+				if os.path.isfile(file_path):
+					shutil.move(file_path, os.path.join(data_folder, folder_names[folder_index]))
+
+
+	def check_ratio_list(self, data_folder, file_list):
+		hypertrophic_count = 0
+		total = 0
+
+		for pickle_file in file_list:
+			with open(os.path.join(data_folder, pickle_file), "rb") as file:
+				loaded_pickle = pickle.load(file)
+				total += 1
+				if loaded_pickle.hypertrophic:
+					hypertrophic_count += 1
+
+		return total, hypertrophic_count, hypertrophic_count / total
+
+	def check_ratio_folder(self, data_folder):
+		pickle_files = sorted(os.listdir(data_folder))
+		total, hypertrophic_count, ratio = self.check_ratio_list(data_folder, pickle_files)
+		print(f'Total files: {total}\nHypertrophic files: {hypertrophic_count}\nRatio: {hypertrophic_count / total}')
--- own_script.py
+++ own_script.py
@@ -1,13 +1,11 @@
-# from con2img import draw_contourmtcs2image as draw
-# from dicom_preprocessor_old import DicomPreProcessorOld
-
 from NNetwork import CNN, NNetworkHelper
+from hyp_file_man import HypertrophyFileManager
 from test_read import TestReader
 from test_rotation import TestRotation
 from vector_average_calc import VectorAverageCalc
 from dicom_preprocessor import DicomPreprocessor
 
-image_folder = '../hypertrophy'
+# image_folder = '../hypertrophy'
 
 # testRot = TestRotation()
 # testRot.rot2()
@@ -22,6 +20,10 @@ image_folder = '../hypertrophy'
 
 # CNN training test
 
-nnet = NNetworkHelper("data")
-nnet.train(25)
-nnet.validate()
+hyp_man = HypertrophyFileManager()
+hyp_man.separate_files("data", (75, 15, 10), ("train", "validate", "test"), 0.1)
+
+
+# nnet = NNetworkHelper("data")
+# nnet.train(25)
+# nnet.validate()
